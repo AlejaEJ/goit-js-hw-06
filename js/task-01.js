2231835542d1bd3117ae16e27bd5f504ceb3b44c
AlejaEJ <alejaespejo96@gmail.com>
2023-09-19 15:40:17 -0500

    Task 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,20 @@
-const categoriesList = document.querySelector('#categories');
+const categoriesList = document.querySelector('ul#categories');
 
 const categoryItems = categoriesList.querySelectorAll('.item');
 
 const numberOfCategories = categoryItems.length;
 
-console.log(`Number of categories: ${numberOfCategories}\n`);
+console.log(`Number of categories: ${numberOfCategories}`);
 
 categoryItems.forEach((categoryItem) => {
 
   const categoryName = categoryItem.querySelector('h2').textContent;
 
-  const categoryElements = categoryItem.querySelectorAll('ul li');
+  const categoryElements = categoryItem.querySelectorAll('li');
 
   const numberOfElements = categoryElements.length;
 
   console.log(`Category: ${categoryName}`);
   
-  console.log(`Elements: ${numberOfElements}\n`);
+  console.log(`Elements: ${numberOfElements}`);
 });
