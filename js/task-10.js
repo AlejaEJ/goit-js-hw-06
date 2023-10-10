92a9701f5faafb020b38945886c5416b5ab640f4
AlejaEJ <alejaespejo96@gmail.com>
2023-10-10 16:03:36 -0500

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,37 @@
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+
+destroyButton.addEventListener('click', destroyBoxes)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function createBoxes() {
+  const amount = input.value;
+
+  boxesContainer.innerHTML = '';
+
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = size + 'px';
+    box.style.height = size + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
 }
